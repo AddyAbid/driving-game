193dfff37d054675543d24b0a95eefc187e27094
Addy Abid <addy.abeed@gmail.com>
2021-11-04 21:43:28 +0000

    Update data model to show which direction the car is facing

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+/* global data */
+/* exported data */
+
 var $car = document.querySelector('.car');
 document.addEventListener('keydown', changeDirection);
 
 function changeDirection(event) {
   if (event.keyCode === 38) {
     $car.className = 'car north';
+    data.direction = 'north';
   } else if (event.keyCode === 37) {
     $car.className = 'car west';
+    data.direction = 'west';
   } else if (event.keyCode === 39) {
     $car.className = 'car east';
+    data.direction = 'east';
   } else if (event.keyCode === 40) {
     $car.className = 'car south';
+    data.direction = 'south';
   }
 }
